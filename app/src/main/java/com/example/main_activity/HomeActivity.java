a35f64351653cd69deb776bd51f0e9c6a1bf93d2
jhivesh Subron <jhivesh.subron@hotmail.com>
2022-11-13 08:09:42 +0400

    added ViewPager2 - implementation viewPagerAdpater with FragmentStateAdapter

--- app/src/main/java/com/example/main_activity/HomeActivity.java
+++ app/src/main/java/com/example/main_activity/HomeActivity.java
@@ -1,21 +1,31 @@
 package com.example.main_activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
 //import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.widget.TableLayout;
 
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class HomeActivity extends AppCompatActivity {
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle toggle;
     NavigationView navigationView;
+    ViewPager2 pager2;
+    TabLayout mTabLayout;
+    TabItem Workout, Calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +34,11 @@ public class HomeActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
 
         setSupportActionBar(toolbar); //set the toolbar to this certain activity
+
+        pager2 = findViewById(R.id.viewpager);
+        mTabLayout = findViewById(R.id.tab_layout);
+        Workout = findViewById(R.id.workout_id);
+        Calendar = findViewById(R.id.calendar_id);
 
         drawerLayout = findViewById(R.id.drawer);
         navigationView = findViewById(R.id.nav_view);
@@ -34,6 +49,21 @@ public class HomeActivity extends AppCompatActivity {
         drawerLayout.addDrawerListener(toggle);
         toggle.setDrawerIndicatorEnabled(true);
         toggle.syncState(); //check if drawer is open or close
+
+        PagerAdapter adapter = new PagerAdapter(this);
+        pager2.setAdapter(adapter);
+
+        new TabLayoutMediator(mTabLayout, pager2,
+                new TabLayoutMediator.TabConfigurationStrategy() {
+                    @Override
+                    public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                        tab.setText("Tab :"+ (position +1));
+
+                    }
+                }
+
+        ).attach();
+
 
     }
 }
---
+++ app/src/main/java/com/example/main_activity/PagerAdapter.java
@@ -0,0 +1,32 @@
+package com.example.main_activity;
+
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class PagerAdapter extends FragmentStateAdapter {
+    public PagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+
+        switch (position){
+            case 0:
+                return new workoutFragment();
+            case 1:
+                return new calendarFrag();
+            default: return null;
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 2;
+    }
+}
